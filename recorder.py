c0efca6b337caa56218617d410b3d108dbbe8457
Moein <44470404+antiZionismmm@users.noreply.github.com>
2023-11-05 17:21:55 +0330

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -22,13 +22,24 @@ def log_data():
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
         cpu_temperature = get_cpu_temperature()
         cpu_percent = psutil.cpu_percent(interval=1)  # Get CPU usage percentage
-        log_entry = f"{timestamp}, CPU Temp: {cpu_temperature}      C, CPU %: {cpu_percent}%"
+
+        # Get network traffic statistics
+        net_stats = psutil.net_io_counters()
+        net_in_mb = (net_stats.bytes_recv * 8) / (1024 * 1024)  # Convert bytes to megabits (Mb)
+        net_out_mb = (net_stats.bytes_sent * 8) / (1024 * 1024)  # Convert bytes to megabits (Mb)
+
+        # Get RAM usage
+        ram_usage = psutil.virtual_memory()
+
+        log_entry = f"{timestamp}, cpuTemp: {cpu_temperature} C, CPU%: {cpu_percent}%, " \
+                    f"NetIn: {net_in_mb} Mb, NetOut: {net_out_mb} Mb, " \
+                    f"RAM%: {ram_usage.percent}%"
 
         with open(log_file, 'a') as file:
             file.write(log_entry + '\n')
 
         print(log_entry)
-        time.sleep(10)  # Log data every 60 seconds
+        time.sleep(10)  # Log data every 10 seconds
 
 if __name__ == "__main__":
     if not os.path.exists(log_file):
